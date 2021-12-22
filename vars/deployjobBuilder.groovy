1bc8c5756c206020020563cb43727bdf0056e775
basilvg-eGov <basil.varghese@egovernments.org>
2021-12-23 01:57:20 +0530

    testing deploy builder

--- vars/deployjobBuilder.groovy
+++ vars/deployjobBuilder.groovy
@@ -45,7 +45,7 @@ spec:
         node(POD_LABEL) {
         
         String url = "git@github.com:egovernments/DIGIT-DevOps.git";
-        String folderdir = "./deploy-as-code/helm/release_charts";
+        String folderdir = "./deploy-as-code/helm/release_charts/*";
         String envdir = "./deploy-as-code/helm/environments";
         def dirs = [];
         def envs = [];
