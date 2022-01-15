be01209d30e59ca5ce86e1192fe0186b57989417
nikesh-eGov <48427967+nikesh-eGov@users.noreply.github.com>
2022-01-15 22:26:21 +0530

    Update deployjobBuilder.groovy

--- vars/deployjobBuilder.groovy
+++ vars/deployjobBuilder.groovy
@@ -137,6 +137,19 @@ spec:
             envs = envs + "]";
         }      
         
+def name = []           
+def modulesname = 'grep name: ../deploy-as-code/helm/product-release-charts/Urban/dependancy_chart-urban-v2.4.yaml'.execute() | [ 'awk', '{print $3}'].execute()
+modulesname.waitFor()
+modulesname = modulesname.text.split( "\\r?\\n" );
+for (int e = 0; e < modulesname.size(); e++ ){
+if(modulesname[e].contains("m_")){
+                    name.add(modulesname[e])
+                    }
+}
+ println name 		
+		
+		
+		
         Set<String> repoSet = new HashSet<>();
         String repoList = "";
 
