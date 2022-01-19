be8ef3f3f389559b5ee0fab3b4735dfc0d3b63dc
gajendran.c <gajendran.c@egovernments.org>
2022-01-19 14:03:45 +0530

    Code try old deployer script

--- vars/deployjobBuilder.groovy
+++ vars/deployjobBuilder.groovy
@@ -1,184 +1,188 @@
-
 import org.egov.jenkins.Utils
 import groovy.io.*
 
 def call(Map params) {
-podTemplate(yaml: """
-  kind: Pod
-  metadata:
-    name: build-utils
-  spec:
-    containers:
-    - name: build-utils
-      image: egovio/build-utils:7-master-95e76687
-      imagePullPolicy: IfNotPresent
-      command:
-      - cat
-      tty: true
-      env:
-        - name: DOCKER_UNAME
-          valueFrom:
-            secretKeyRef:
-              name: jenkins-credentials
-              key: dockerUserName
-        - name: DOCKER_UPASS
-          valueFrom:
-            secretKeyRef:
-              name: jenkins-credentials
-              key: dockerPassword
-        - name: DOCKER_NAMESPACE
-          value: egovio
-        - name: DOCKER_GROUP_NAME
-          value: dev
-      resources:
-        requests:
-          memory: "256Mi"
-          cpu: "200m"
-        limits:
-          memory: "256Mi"
-          cpu: "200m"
-""")
+    podTemplate(yaml: """
+                kind: Pod
+                metadata:
+                  name: build-utils
+                spec:
+                  containers:
+                  - name: build-utils
+                    image: egovio/build-utils:7-master-95e76687
+                    imagePullPolicy: IfNotPresent
+                    command:
+                    - cat
+                    tty: true
+                    env:
+                      - name: DOCKER_UNAME
+                        valueFrom:
+                          secretKeyRef:
+                            name: jenkins-credentials
+                            key: dockerUserName
+                      - name: DOCKER_UPASS
+                        valueFrom:
+                          secretKeyRef:
+                            name: jenkins-credentials
+                            key: dockerPassword
+                      - name: DOCKER_NAMESPACE
+                        value: egovio
+                      - name: DOCKER_GROUP_NAME  
+                        value: dev
+                    resources:
+                      requests:
+                        memory: "256Mi"
+                        cpu: "200m"
+                      limits:
+                        memory: "256Mi"
+                        cpu: "200m"                
+      """)
+      {
+        node(POD_LABEL) {
+        String url = params.url;
+        String folderdir = './deploy-as-code/helm/product-release-charts';
+        String envdir = './deploy-as-code/helm/environments';
+        def dirs = [];
+        def envFiles = []
+        def tmp_file = ".files_list"
+        Map<String,List<String>> mapVersions = new HashMap<>();
+        Map<String,List<String>> mapFeatureModules = new HashMap<>();
+        Map<String,List<String>> mapCoreModuls = new HashMap<>();
+        StringBuilder jobDslScript = new StringBuilder();
+        String sProducts = "[";
+        List<String> lTargetEnvs = []    
+        String dirName = Utils.getDirName(url);
+        dir(dirName) {
+            git url: url, credentialsId: 'git_read'
 
-  {
-    node(POD_LABEL) {
-      String url = params.url
-      String releaseChartDir = "./deploy-as-code/helm/product-release-charts"
-      String envdir = "./deploy-as-code/helm/environments"
-      List lTargetEnvs = []
-      List lProducts = []
-      def tmp_file = ".files_list"
-      def mapProductsVersionsModules = []
+            // Read products
+            sh "ls ${folderdir} > ${tmp_file}"
+            dirs = readFile(tmp_file).split( "\\r?\\n" );
+            sh "rm -f ${tmp_file}"
 
-      StringBuilder jobDslScript = new StringBuilder()
-      StringBuilder html_to_be_rendered = new StringBuilder()
-
-      String sReleaseChartsDirPath = Utils.getDirName(url)
-      dir(sReleaseChartsDirPath) {
-          git url: url, credentialsId: "git_read"
-
-          //Read the env files(i) list
-
-          sh "ls ${envdir} > ${tmp_file}"
-          lAllEnvs = readFile(tmp_file).split( "\\r?\\n" )
-          sh "rm -f ${tmp_file}"
-          for (int i = 0; i < lAllEnvs.size(); i++) {
-              if (!lAllEnvs[i].contains("secrets") && !lAllEnvs[i].contains("ci")) {
-                lTargetEnvs.add(lAllEnvs[i].substring(0, lAllEnvs[i].indexOf(".yaml")))
-              }
-          }
-
-          //Read the Charts dir list
-          
-          sh "ls ${releaseChartDir} > ${tmp_file}"
-          lProducts = readFile(tmp_file).split( "\\r?\\n" )
-          sh "rm -f ${tmp_file}"
-          
-          //Read all the Product(i) Folder and Go inside to read the chart(j), version(k) and modules (e)
-          for (int i = 0; i < lProducts.size(); i++) {
-
-              def lChartFiles = []
-              sh "ls ${releaseChartDir}/${lProducts[i]} > ${tmp_file}"
-              lChartFiles = readFile(tmp_file).split( "\\r?\\n" )
-              sh "rm -f ${tmp_file}"
-              lChartFiles.sort()
-
-              Map<String, List<String>> mapCoreModules = new HashMap<>()
-              Map<String, List<String>> mapFeatureModules = new HashMap<>()
-              Map<String, mapCoreModules, mapFeatureModules> mapVersionsModules = new HashMap<>()
-
-              // Extract the modules from the chart
-              for (int k = 0; k < lChartFiles.size(); k++) {
-                  def lTotalModules = []
-                  Map<String, List<String>> featureModules = new HashMap<>()
-                  Map<String, List<String>> coreModules = new HashMap<>()
-
-                  sh "grep 'name:' ${releaseChartDir}/${lProducts[i]}/${lChartFiles[k]} | cut -d' ' -f7 > ${tmp_file}"
-                  lTotalModules = readFile(tmp_file).split( "\\r?\\n" )
-                  sh "rm -f ${tmp_file}"
-
-                  //Extract the core and feature modules from each speific versions
-                  for (int e = 0; e < lTotalModules.size(); e++ ) {
-
-                      if (lTotalModules[e].contains("m_")) {
-                          featureModules.add("FeatureModules", lTotalModules[e])
-                      } else {
-                          coreModules.add("CoreModules", lTotalModules[e])
-                      }
-                  }
-                mapVersionsModules.add(lChartFiles[k].substring(lChartFiles[k].indexOf("-") + 1, lChartFiles[k].indexOf(".y")), coreModules, featureModules)
-              }          
-              mapProductsVersionsModules.add(lProducts[i], mapVersionsModules)
-          }
-      }
-
-
-      jobDslScript.append("""
-            pipelineJob("self-provision/deploy") {
-                description()
-                keepDependencies(false)
-                parameters {
-
-                  activeChoiceParam('Environments') {
-                        description('Choose the Intended environment to deploy')
-                        filterable(false)
-                        choiceType('SINGLE_SELECT')
-                        groovyScript {
-                            script(''' ${lTargetEnvs} ''')
-                            fallbackScript('"fallback choice"')
-                        }
-                    }
-
-                    activeChoiceParam('Project') {
-                        description('choose Project from multiple choices')
-                        filterable(false)
-                        choiceType('SINGLE_SELECT')
-                        groovyScript {
-                            script(''' ${lProducts} ''')
-                            fallbackScript('"fallback choice"')
-                        }
-                    }
-
-                    activeChoiceReactiveParam('Release-Version') {
-                        description('choose release chart version from multiple choices')
-                        filterable(false)
-                        choiceType('SINGLE_SELECT')
-                        groovyScript {
-                            script(''' return ${mapVersionsModules.inspect()}.get(Project) ''')
-                            fallbackScript('"fallback choice"')
-                        }
-                        referencedParameter('Project')
-                    }
-
-                    activeChoiceReactiveParam('Services') {
-                        description('Select the intended modules to be Installed')
-                        filterable(false)
-                        choiceType('CHECKBOX')
-                        groovyScript {
-                            script(''' return ${mapProductsVersionsModules.inspect()}.get(Release-Version).get(Project) ''')   
-                            fallbackScript('"fallback choice"')
-                        }
-                        referencedParameter('Release-Version')
-                    }
-                    booleanParam("Cluster_Configs", false, "Whenever you made changes to the deployment conifg ensure the cluster_config check is checked to pick the latest configs from the deployment")
-                    booleanParam("Print_Manifest", true, "Whenever you want to deployment manifest ensure the uncheck checked box")
+            for (int i = 0; i < dirs.size(); i++) {
+                sProducts = sProducts + "\"" + dirs[i] + "\"";
+                if(i!=dirs.size()-1){
+                      sProducts = sProducts + ",";
                 }
-                definition {
-                  cps {
-                      script('''library 'ci-libs'
-                        selfDeployer(repo:'git@github.com:egovernments/DIGIT-DevOps.git', branch: 'master', helmDir: 'deploy-as-code/helm')''')
-                      sandbox()
-                  }
-                }
-                disabled(false)
             }
-      """);
+            sProducts = sProducts + "]";
 
-      stage('Building jobs') {
-        sh """
-              echo \"Job DsL Script:  ${jobDslScript.toString()}\"
-          """
-        jobDsl scriptText: jobDslScript.toString()
-      }
+            // Read Envs
+            sh "ls ${envdir} > ${tmp_file}"
+            lAllEnvs = readFile(tmp_file).split( "\\r?\\n" )
+            sh "rm -f ${tmp_file}"
+            for (int i = 0; i < lAllEnvs.size(); i++) {
+                if (!lAllEnvs[i].contains("secrets") && !lAllEnvs[i].contains("ci")) {
+                  lTargetEnvs.add(lAllEnvs[i].substring(0, lAllEnvs[i].indexOf(".yaml")))
+                }
+            }
+
+            for (int i = 0; i < dirs.size(); i++) {
+                def subfolderlist = []
+                def subFiles = []
+                sh "ls ${folderdir}/${dirs[i]} > ${tmp_file}"
+                subfolderlist = readFile(tmp_file).split( "\\r?\\n" );
+                sh "rm -f ${tmp_file}"
+
+                for (int j = 0; j < subfolderlist.size(); j++) {
+                    subFiles.add(subfolderlist[j].substring(subfolderlist[j].indexOf("-")+1,subfolderlist[j].indexOf(".y")))
+                }
+                subFiles = subFiles.sort()
+                mapVersions.put(dirs[i], subFiles)
+                for (int k = 0; k < subFiles.size(); k++){
+                    def lFeatureModules = []
+                    def lCoreModules = []
+                    sh "grep 'name:' ${folderdir}/${dirs[i]}/dependancy_chart-${subFiles[k]}.yaml | cut -d' ' -f7 > ${tmp_file}"
+                    modulesname = readFile(tmp_file).split( "\\r?\\n" );
+                    sh "rm -f ${tmp_file}"
+
+			              for (int e = 0; e < modulesname.size(); e++ ){
+                      if(modulesname[e].contains("m_")){
+                          lFeatureModules.add(modulesname[e])
+                      } else {
+                          lCoreModules.add(modulesname[e])
+                      }
+                      mapFeatureModules.put(subFiles[k], lFeatureModules)
+                      mapCoreModules.put(subFiles[k], lCoreModules)  
+                    }
+                }  
+            }
+
+        }      
+    
+
+        jobDslScript.append("""
+          pipelineJob("self-provision/deploy") {
+            description()
+            keepDependencies(false)
+            parameters {
+                activeChoiceParam('Environments') {
+                    description('choose environment from multiple choices')
+                    filterable(false)
+                    choiceType('SINGLE_SELECT')
+                    groovyScript {
+                        script(''' ${lTargetEnvs} ''')
+                        fallbackScript('"fallback choice"')
+                    }
+                }
+                activeChoiceParam('Project') {
+                    description('choose Project from multiple choices')
+                    filterable(false)
+                    choiceType('SINGLE_SELECT')
+                    groovyScript {
+                        script(''' ${sProducts} ''')
+                        fallbackScript('"fallback choice"')
+                    }
+                }
+                activeChoiceReactiveParam('Release-Version') {
+                    description('choose release chart version from multiple choices')
+                    filterable(false)
+                    choiceType('SINGLE_SELECT')
+                    groovyScript {
+                        script(''' 
+                        def testmap = ${mapVersions.inspect()}
+                            return testmap.get(Project)''')
+                        fallbackScript('"fallback choice"')
+                    }
+                    referencedParameter('Project')	
+                }
+                activeChoiceReactiveParam('Modules') {
+                    description('choose Modules from release chart from multiple choices')
+                    filterable(false)
+                    choiceType('CHECKBOX')
+                    groovyScript {
+                        script(''' 
+                        def mapFeatureModules = ${mapFeatureModules.inspect()}
+                        return mapFeatureModules.get(Release-Version)''')
+                        fallbackScript('"fallback choice"')
+                    }
+                    referencedParameter('Release-Version')	
+                } 
+                booleanParam("Cluster_Configs", false, "Whenever you made changes to the deployment conifg ensure the cluster_config check is checked to pick the latest configs from the deployment")
+                booleanParam("Print_Manifest", true, "Whenever you want to deployment manifest ensure the uncheck checked box")
+            }  
+
+            definition {
+              cps {
+                  script('''library 'ci-libs'
+                              selfDeployer(repo:'git@github.com:egovernments/DIGIT-DevOps.git', branch: 'master', helmDir: 'deploy-as-code/helm')''')
+                  sandbox() 
+                }
+            }
+            disabled(false)
+          }            
+        """);
+        
+        stage('Building jobs') {
+          sh """
+            echo \"Job DsL Script:  ${jobDslScript.toString()}\"
+            """
+            jobDsl scriptText: jobDslScript.toString()
+            
+        }              
+
     }
+
   }
 }
