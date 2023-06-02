602407a76d1ebabca1be73b95cb6d1f01d828160
SriPadma8997-egov <sri.padma@egovernments.org>
2023-06-02 13:48:37 +0530

    updated buildpipeline and jobbuilder

--- vars/jobBuilder.groovy
+++ vars/jobBuilder.groovy
@@ -29,7 +29,7 @@ spec:
             name: jenkins-credentials
             key: dockerPassword
       - name: DOCKER_NAMESPACE
-        value: {{DOCKER_ACCOUNT}}
+        value: sripadma8997
       - name: DOCKER_GROUP_NAME  
         value: dev
     resources:
