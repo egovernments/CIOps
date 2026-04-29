e1cef6d9a2470b2991b77b285e97f96af4f087a6
shivam-eGov <shivam.upadhyay@egovernments.org>
2026-04-29 16:11:08 +0530

    fixed working directory in deployer image

--- vars/deployer.groovy
+++ vars/deployer.groovy
@@ -11,7 +11,7 @@ spec:
     fsGroup: 1000
   containers:
   - name: egov-deployer
-    image: egovio/egov-deployer:vapt-security-check-1
+    image: egovio/egov-deployer:vapt-security-check-2
     command:
     - cat
     tty: true
